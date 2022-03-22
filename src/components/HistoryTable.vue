8863c466d3e5c97c72a7fb6e358703893bcacc9b
heamongus <heamongus@mail.ru>
2022-03-22 20:44:14 +0300

    Lesson 15

---
+++ src/components/HistoryTable.vue
@@ -0,0 +1,59 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Сумма</th>
+        <th>Дата</th>
+        <th>Категория</th>
+        <th>Тип</th>
+        <th>Открыть</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(record, index) in records" :key="record.id">
+        <td>{{ index + 1 }}</td>
+        <td>{{ currencyFilter(record.amount) }}</td>
+        <td>{{ dateFilter(record.date, "datetime") }}</td>
+        <td>{{ record.categoryName }}</td>
+        <td>
+          <span class="white-text badge" :class="[record.typeClass]">{{
+            record.typeText
+          }}</span>
+        </td>
+        <td>
+          <button
+            v-tooltip="'Открыть запись'"
+            class="btn-small btn"
+            @click="$router.push('/detail/' + record.id)"
+          >
+            <i class="material-icons">open_in_new</i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import currencyFilter from "@/filters/currencyFilter";
+import dateFilter from "@/filters/dateFilter";
+export default {
+  name: "HistoryTable",
+  props: {
+    records: {
+      required: true,
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      currencyFilter: currencyFilter,
+      dateFilter: dateFilter,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/History.vue
+++ src/views/History.vue
@@ -8,43 +8,50 @@
       <canvas></canvas>
     </div>
 
-    <section>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Сумма</th>
-            <th>Дата</th>
-            <th>Категория</th>
-            <th>Тип</th>
-            <th>Открыть</th>
-          </tr>
-        </thead>
+    <loader v-if="isLoading" />
 
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>1212</td>
-            <td>12.12.32</td>
-            <td>name</td>
-            <td>
-              <span class="white-text badge red">Расход</span>
-            </td>
-            <td>
-              <button class="btn-small btn">
-                <i class="material-icons">open_in_new</i>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <p class="center" v-else-if="!records.length">
+      Записи не найдены.<router-link to="/record"
+        >Добавить новую запись</router-link
+      >
+    </p>
+
+    <section v-else>
+      <history-table :records="records" />
     </section>
   </div>
 </template>
 
 <script>
+import HistoryTable from "../components/HistoryTable";
+import Loader from "../components/app/Loader";
 export default {
   name: "History",
+  components: { Loader, HistoryTable },
+
+  data() {
+    return {
+      isLoading: true,
+      records: [],
+      categories: [],
+    };
+  },
+  async mounted() {
+    const records = await this.$store.dispatch("fetchRecords");
+    this.categories = await this.$store.dispatch("fetchCategories");
+
+    this.records = records.map((record) => {
+      return {
+        ...record,
+        categoryName: this.categories.find((c) => c.id === record.categoryId)
+          .title,
+        typeClass: record.type === "income" ? "green" : "red",
+        typeText: record.type === "income" ? "Доход" : "Расход",
+      };
+    });
+
+    this.isLoading = false;
+  },
 };
 </script>
 
